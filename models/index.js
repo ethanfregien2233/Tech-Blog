1da30910231a98e48b99d5c04885ee129169005b
ethan fregien <ethanfregien@gmail.com>
2022-01-20 18:07:04 -0600

    setup most of js

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {foreignKey: 'user_id', onDelete: 'CASCADE'});
+Post.belongsTo(User, {foreignKey: 'user_id', onDelete: 'SET NULL'});
+
+Post.hasMany(Comment, {foreignKey: 'post_id', onDelete: 'CASCADE'});
+Comment.belongsTo(Post, {foreignKey: 'post_id', onDelete: 'SET NULL'})
+
+User.hasMany(Comment, {foreignKey: 'user_id', onDelete: 'CASCADE'});
+Comment.belongsTo(User, {foreignKey: 'user_id', onDelete: 'SET NULL'});
+
+
+module.exports = { User, Post, Comment };
---
+++ public/add-post.js
@@ -0,0 +1,21 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+    const title = document.querySelector('input[name="post-title"]').value;
+    const contents = document.querySelector('input[name="post-contents"]').value;
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST', 
+        body: JSON.stringify({
+            title, contents
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if(response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.newpost-form').addEventListener('submit', newFormHandler);
